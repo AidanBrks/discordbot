0b6bc85099ea298c571172bdeaa68f2e10ba66e4
Aidan <brooksal01@gmail.com>
2021-04-20 23:37:23 +0100

    Setup command functionality..

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const fs = require('fs')
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 require('dotenv').config()
+
+const commandfiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+
+for (const file of commandfiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -11,10 +21,21 @@ client.on('ready', () => {
 
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
-    }
+client.on('message', message => {
+	if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
+
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
+
 
 client.login(process.env.CLIENT_TOKEN);
